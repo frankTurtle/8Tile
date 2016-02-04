d725c75cd73eab1311b995c571dc195105777fb2
Barret J. Nobel <bear.nobel@gmail.com>
2016-02-03 21:55:25 -0500

    added comments

--- src/TileBoard.java
+++ src/TileBoard.java
@@ -9,31 +9,42 @@
  */
 public class TileBoard {
 
-    private final int EMPTY_SPACE = -1;
-    private int[][] board;
+    private final int EMPTY_SPACE = -1; //........ variable for the empty space
+    private int[][] board; //..................... multidimensional array to the location of all spaces on the board
 
+    // Default constructor
     public TileBoard() {
         this.board = new int[][] {
                 {1,2,3},
                 {8,EMPTY_SPACE,4},
                 {7,6,5}
-        };
+        }; //........................ instantiates the board and populates with default values in the correct order
     }
 
+    // Constructor with parameters
+    // takes in a multidimensional array
+    // instantiates local board with values passed in
+    // Formatted as:
+    // Current Board
+    // Row 1: 1 2 3
+    // Row 2: 8 X 4
+    // Row 3: 7 6 5
     public TileBoard( int[][] arrayIn ){
-        this.board = new int[3][3];
+        this.board = new int[3][3]; //........................................ instantiates the board to be 3x3
 
-        for( int i = 0; i < board.length; i++ )
-            for( int j = 0; j < board[i].length; j++ )
-                board[i][j] = arrayIn[i][j];
+        for( int i = 0; i < board.length; i++ ) //............................ loops through each row
+            System.arraycopy(arrayIn[i], 0, board[i], 0, board[i].length); //. copies values from array passed in
     }
 
+    // Overridden toString
+    // prints the board as it is currently
     public String toString(){
-        String returnString = "Current Board";
-        for( int i = 0; i < this.board.length; i++ ){
-            returnString += "\nRow " + (i+1) + ": ";
-            for( int number : this.board[i] ){
-                returnString += ( number > 0 ) ? number + " " : "X ";
+        String returnString = "Current Board"; //......................... create String to return
+
+        for( int i = 0; i < this.board.length; i++ ){ //.................. loops through each row
+            returnString += "\nRow " + (i+1) + ": "; //................... prints a header for each row with the row number
+            for( int number : this.board[i] ){ //......................... loops through each column
+                returnString += ( number > 0 ) ? number + " " : "X "; //.. concatenate the number, and put an X if its the empty space
             }
         }
 
