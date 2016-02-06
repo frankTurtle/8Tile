5959468fc849eaaa21676ff52c66cffee3017808
Barret J. Nobel <bear.nobel@gmail.com>
2016-02-05 21:25:54 -0500

    created two files one creates an ArrayList of button objects the other is the GUI app itself

---
+++ src/BoardButtonFactory.java
@@ -0,0 +1,43 @@
+/**
+ * Class BoardButtonFactory
+ * This class creates all the buttons for the gamefield
+ * Created: 02/05/16
+ * Updated: 02/05/16
+ * Author: Barret J. Nobel
+ * Contact: bear.nobel at gmail
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BoardButtonFactory {
+    private static ArrayList<JButton> jButtons; //....................................................... list of buttons
+
+    // Default constructor
+    // creates a perfect board
+    public BoardButtonFactory(){
+        this( new int[][] { {1,2,3},
+                            {8,-1,4},
+                            {7,6,5} }  );
+    }
+
+    // Constructor with parameters
+    // takes a multidimensional array in and populates the buttons
+    // also increases font size for buttons
+    public BoardButtonFactory( int[][] labels ){
+        jButtons = new ArrayList<>(); //..................................................................... instantiate the ArrayList
+        for( int row = 0; row < labels.length; row++ ){ //................................................... loop through each row
+            for( int label : labels[row] ){ //............................................................... loop through each column
+                JButton addThisButton = ( label > 0 ) ? new JButton( "" + label ) : new JButton( "X" ); //... create a button with label from array - X if its the space
+                addThisButton.setFont(new Font("Arial", Font.PLAIN, 40)); //................................. increase font of button
+                jButtons.add( addThisButton ); //............................................................ add buttons to ArrayList
+            }
+        }
+    }
+
+    // Method to return the ArrayList full of buttons
+    public static ArrayList<JButton> getJButtons(){
+        return jButtons;
+    }
+}
---
+++ src/EightTileGUI.java
@@ -0,0 +1,113 @@
+/**
+ * Class EightTileGUI
+ * This class to give it a GUI and user interaction
+ * Created: 02/05/16
+ * Updated: 02/05/16
+ * Author: Barret J. Nobel
+ * Contact: bear.nobel at gmail
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class EightTileGUI {
+
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel gameButtons;
+    private JPanel rightSide;
+    private JPanel searchTypes;
+    private JTextArea textField;
+    private JLabel hintLabel;
+    private JButton generateButton;
+
+    public EightTileGUI(){
+        prepareGUI();
+    }
+
+    public static void main(String[] args){
+        EightTileGUI swingControlDemo = new EightTileGUI();
+        swingControlDemo.showEventDemo();
+    }
+
+    private void prepareGUI(){
+        mainFrame = new JFrame("EightTile Game");
+        mainFrame.setSize(500,500);
+        mainFrame.setLayout(new GridLayout(0, 2));
+
+        headerLabel = new JLabel("",JLabel.CENTER );
+        statusLabel = new JLabel("",JLabel.CENTER);
+
+        statusLabel.setSize(350,100);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        gameButtons = new JPanel();
+        gameButtons.setLayout(new GridLayout(3,3));
+
+        for( JButton button : BoardButtonFactory.getJButtons() ){
+            gameButtons.add( button );
+        }
+
+        rightSide = new JPanel();
+        rightSide.setLayout( new GridLayout(4,1) );
+
+        searchTypes = new JPanel();
+        searchTypes.setLayout( new GridLayout(0,2) );
+
+        searchTypes.add( new JButton("Breadth") );
+        searchTypes.add( new JButton("Depth") );
+
+        textField = new JTextArea();
+        textField.setEditable(false);
+
+        hintLabel = new JLabel( "Hint", SwingConstants.CENTER );
+
+        generateButton = new JButton("GENERATE");
+
+        rightSide.add(searchTypes);
+        rightSide.add(textField);
+        rightSide.add(hintLabel);
+        rightSide.add(generateButton);
+
+        mainFrame.add(gameButtons);
+        mainFrame.add(rightSide);
+    }
+
+    private void showEventDemo(){
+        headerLabel.setText("Control in action: Button");
+
+        JButton okButton = new JButton("OK");
+        JButton submitButton = new JButton("Submit");
+        JButton cancelButton = new JButton("Cancel");
+
+        okButton.setActionCommand("OK");
+        submitButton.setActionCommand("Submit");
+        cancelButton.setActionCommand("Cancel");
+
+        okButton.addActionListener(new ButtonClickListener());
+        submitButton.addActionListener(new ButtonClickListener());
+        cancelButton.addActionListener(new ButtonClickListener());
+
+        mainFrame.setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if( command.equals( "OK" ))  {
+                statusLabel.setText("Ok Button clicked.");
+            }
+            else if( command.equals( "Submit" ) )  {
+                statusLabel.setText("Submit Button clicked.");
+            }
+            else  {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
+}
