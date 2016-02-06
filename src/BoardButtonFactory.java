7ebaf04bd391c4a105e0dbe0f99e0560c367ffc6
Barret J. Nobel <bear.nobel@gmail.com>
2016-02-06 11:24:24 -0500

    game is fully functional with clicking buttons

--- src/BoardButtonFactory.java
+++ src/BoardButtonFactory.java
@@ -12,14 +12,15 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class BoardButtonFactory {
-    private static ArrayList<JButton> jButtons; //....................................................... list of buttons
+    private static ArrayList<JButton> jButtons; //................. list of buttons
+    private static JButton[][] jButtonsArray; //................... array of buttons
+    private static int[][] labelsArray; //......................... array of labels
+    private static TileBoard jBoard; //............................ TileBoard representation
 
     // Default constructor
     // creates a perfect board
     public BoardButtonFactory(){
-        this( new int[][] { {1,2,3},
-                            {8,-1,4},
-                            {7,6,5} }  );
+        this( new TileBoard() );
     }
 
     // Constructor with parameters
@@ -27,6 +28,10 @@ public class BoardButtonFactory {
     // also increases font size for buttons
     public BoardButtonFactory( int[][] labels ){
         jButtons = new ArrayList<>(); //................................................................................................. instantiate the ArrayList
+        jButtonsArray = new JButton[3][3]; //............................................................................................ instantiate the Array
+        jBoard = new TileBoard( labels ); //............................................................................................. instantiate the TileBoard
+        labelsArray = new int[3][3];
+
         String[][] actionCommands = new String[][]{
                 {"tl", "tm", "tr"},
                 {"ml", "mm", "mr"},
@@ -38,8 +43,11 @@ public class BoardButtonFactory {
                 JButton addThisButton = ( labels[row][column] > 0 ) ? new JButton( "" + labels[row][column] ) : new JButton( "X" ); //... create a button with label from array - X if its the space
                 addThisButton.setFont(new Font("Arial", Font.PLAIN, 40)); //............................................................. increase font of button
                 addThisButton.setEnabled( enableButton(labels, row, column) ); //........................................................ only enables buttons allowed to move to
+                if( !addThisButton.isEnabled() && labels[row][column] > 0 ) addThisButton.setFont( new Font( "Arial", Font.ITALIC, 25));
                 addThisButton.setActionCommand( actionCommands[row][column] ); //........................................................ set the action command
                 jButtons.add( addThisButton ); //........................................................................................ add buttons to ArrayList
+                jButtonsArray[row][column] = addThisButton; //........................................................................... add buttons to Array
+                labelsArray[row][column] = labels[row][column]; //....................................................................... keeps track of the labels
             }
         }
     }
@@ -53,6 +61,21 @@ public class BoardButtonFactory {
     // Method to return the ArrayList full of buttons
     public ArrayList<JButton> getJButtonsAsAList(){
         return jButtons;
+    }
+
+    // Method to retun as multidimensional array
+    public JButton[][] getJButtonsAsArray(){
+       return jButtonsArray;
+    }
+
+    // Method to return the labels array
+    public int[][] getLabels(){
+        return labelsArray;
+    }
+
+    // Method to return the TileBoard object
+    public TileBoard getjBoard(){
+        return jBoard;
     }
 
     // Method to only enable buttons allowed to move to
@@ -71,7 +94,6 @@ public class BoardButtonFactory {
             if( currentLocation.equals(tempBlank) ) //............................... if they're the same enable the button
                 return true;
         }
-
         return false; //............................................................. if none are true don't enable the button
     }
 }
--- src/EightTileGUI.java
+++ src/EightTileGUI.java
@@ -9,19 +9,15 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class EightTileGUI {
 
-    private JFrame mainFrame;
-    private JLabel headerLabel;
-    private JLabel statusLabel;
     private JPanel gameButtons;
-    private JPanel rightSide;
-    private JPanel searchTypes;
     private JTextArea textField;
-    private JLabel hintLabel;
-    private JButton generateButton;
+    private BoardButtonFactory buttonFactory;
+    private ArrayList<JButton> buttonBoardList;
 
     public EightTileGUI(){
         prepareGUI();
@@ -31,67 +27,159 @@ public class EightTileGUI {
         EightTileGUI swingControlDemo = new EightTileGUI();
     }
 
+    // Method to setup the GUI
+    // Frame is 500x500
     private void prepareGUI(){
-        mainFrame = new JFrame("EightTile Game");
-        mainFrame.setSize(500,500);
-        mainFrame.setLayout(new GridLayout(0, 2));
+        JFrame mainFrame = new JFrame("EightTile Game"); //.................... creates a frame to hold everything
+        mainFrame.setSize(500,500); //......................................... sets frame size
+        mainFrame.setLayout(new GridLayout(0, 2)); //.......................... there's two sections, game buttons and everything else
 
-        mainFrame.addWindowListener(new WindowAdapter() {
+        mainFrame.addWindowListener(new WindowAdapter() { //................... listener to close game
             public void windowClosing(WindowEvent windowEvent){
                 System.exit(0);
             }
         });
-        gameButtons = new JPanel();
-        gameButtons.setLayout(new GridLayout(3,3));
 
-        for( JButton button : new BoardButtonFactory().getJButtonsAsAList() ){
-            button.addActionListener( new ButtonClickListener() );
-            gameButtons.add( button );
-        }
+        // ******** LEFT SIDE ************ //
+        gameButtons = new JPanel(); //.................................. panel to hold all buttons in a grid
+        gameButtons.setLayout(new GridLayout(3,3)); //.................. 3x3 layout
+        buttonFactory = new BoardButtonFactory(); //.................... new button factory object
+        buttonBoardList = buttonFactory.getJButtonsAsAList(); //........ instantiate the list of button object for reference
+        addButtonsTo( gameButtons, buttonBoardList ); //................ adds all buttons to gameButtons panel and adds listeners
 
-        rightSide = new JPanel();
-        rightSide.setLayout( new GridLayout(4,1) );
+        // ********** RIGHT SIDE *********** //
+        JPanel rightSide = new JPanel(); //............................. new panel to hold all items on right side
+        rightSide.setLayout( new GridLayout(4,1) ); //.................. 4x1 grid
 
-        searchTypes = new JPanel();
-        searchTypes.setLayout( new GridLayout(0,2) );
-
-        searchTypes.add( new JButton("Breadth") );
+        // ********** SEARCH TYPE ************ //
+        JPanel searchTypes = new JPanel(); //........................... panel to add to right side with search type buttons
+        searchTypes.setLayout( new GridLayout(0,2) ); //................ layout for button grid 0x2
+        searchTypes.add( new JButton("Breadth") ); //................... add buttons to panel
         searchTypes.add( new JButton("Depth") );
 
-        textField = new JTextArea();
-        textField.setEditable(false);
+        // ************* TEXT AREA ************* //
+        textField = new JTextArea(); //.................................. text area to display results
+        textField.setEditable(false); //................................. don't allow user to edit the area
 
-        hintLabel = new JLabel( "Hint", SwingConstants.CENTER );
+        // ************* HINT LABEL ****************** //
+        JLabel hintLabel = new JLabel("Hint", SwingConstants.CENTER); //. creates hint label
 
-        generateButton = new JButton("GENERATE");
+        // ************* GENERATE NEW BOARD BUTTON ************* //
+        JButton generateButton = new JButton("GENERATE");
 
-        rightSide.add(searchTypes);
+        rightSide.add(searchTypes); //.................................... add all components to the panel on the right side
         rightSide.add(textField);
         rightSide.add(hintLabel);
         rightSide.add(generateButton);
 
-        mainFrame.add(gameButtons);
+        mainFrame.add(gameButtons); //.................................... add the right and left panels to the mainFrame
         mainFrame.add(rightSide);
 
-        mainFrame.setVisible(true);
+        mainFrame.setVisible(true); //.................................... display it!
     }
 
+    // Method to deal with the gamepad buttons being clicked
+    // updates the game board and available options
     private class ButtonClickListener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             switch( e.getActionCommand() ){
                 case "tl":
                     textField.setText( "TL button clicked");
+                    updateButtons(0,0);
                     break;
 
                 case "tm":
                     textField.setText( "TM button clicked");
-                    System.out.print("TM button clicked");
+                    updateButtons(0,1);
                     break;
 
-                default:
-                    System.out.print(e.getActionCommand());
+                case "tr":
+                    textField.setText( "Tr button clicked");
+                    updateButtons(0,2);
+                    break;
+
+                case "ml":
+                    textField.setText( "ml button clicked");
+                    updateButtons(1,0);
+                    break;
+
+                case "mm":
+                    textField.setText( "mM button clicked");
+                    updateButtons(1,1);
+                    break;
+
+                case "mr":
+                    textField.setText( "mr button clicked");
+                    updateButtons(1,2);
+                    break;
+
+                case "bl":
+                    textField.setText( "bl button clicked");
+                    updateButtons(2,0);
+                    break;
+
+                case "bm":
+                    textField.setText( "bm button clicked");
+                    updateButtons(2,1);
+                    break;
+
+                case "br":
+                    textField.setText( "br button clicked");
+                    updateButtons(2,2);
                     break;
             }
         }
+    }
+
+    // Method called to update the buttons for the gameboard
+    private void updateButtons( int x, int y ){
+        for( JButton btn : buttonBoardList )gameButtons.remove(btn); //.......... removes all current buttons
+        TileBoard tempBoard = new TileBoard( buttonFactory.getLabels() ); //..... creates a temporary board
+        tempBoard.move( directionFromEmpty(new Point(x,y)) ); //................. moves the temporary board the direction the user pressed
+        buttonFactory = new BoardButtonFactory( tempBoard ); //.................. update the button factory to newest moved coordinates
+        buttonBoardList = buttonFactory.getJButtonsAsAList(); //................. update list of buttons
+
+        addButtonsTo( gameButtons, buttonBoardList ); //......................... add all new buttons back onto the board
+
+        gameButtons.revalidate(); //............................................. update and repaint all buttons
+        gameButtons.repaint();
+    }
+
+    // Method to add all buttons from a list to the panel
+    // also adds the listeners for each button
+    private void addButtonsTo( JPanel panel, ArrayList<JButton> buttonList ){
+        for( JButton addBtn: buttonList ){ //................................. loop through each button in list
+            addBtn.addActionListener( new ButtonClickListener() ); //......... add listener
+            panel.add( addBtn ); //........................................... add to panel to be displayed
+        }
+    }
+
+    // Method to return a string with the direction the user clicked on
+    private String directionFromEmpty( Point location ){
+        Point currentEmpty = new Point( buttonFactory.getjBoard().getLocationOfEmptySpace() ); //............ the location of the current empty point
+
+        for( int row = 0; row < buttonFactory.getJButtonsAsArray().length; row++ ){ //....................... loop through each button board
+            for( int column = 0; column < buttonFactory.getJButtonsAsArray()[row].length; column++ ){
+                JButton currentButton = buttonFactory.getJButtonsAsArray()[row][column]; //.................. temporary button of currently indexed
+
+                if( currentButton.isEnabled() ){ //.......................................................... if its enabled, determine direction in relation to empty point
+                    if( currentEmpty.x > location.x ) {
+                        return "n";
+                    }
+                    else if( currentEmpty.x < location.x ){
+                        return "s";
+                    }
+                    else if( currentEmpty.y > location.y ){
+                        return "w";
+                    }
+
+                    else if( currentEmpty.y < location.y ){
+                        return "e";
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
